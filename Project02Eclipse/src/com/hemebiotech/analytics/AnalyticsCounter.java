22344e8008a497f0be19b2f66b024410f178ce9a
Upeaval <Upeaval@Upeaval.com>
2022-12-20 10:13:16 +0100

    Modification de path

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,7 +4,7 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		// Read the Symptoms.txt file and stock value in map
-		ReadSymptomDataFromFile Read = new ReadSymptomDataFromFile("C:/Users/Thibault Carabasse/OneDrive - ITBRM Consulting/Bureau/OpenClassrooms/P4_Carabasse_Thibault/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt");
+		ReadSymptomDataFromFile Read = new ReadSymptomDataFromFile("C:/Users/Thiba/Desktop/OpenClassrooms/P4/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt");
 		// With the return map, write new file with symptoms and count of him
 		new WriteResultToFile(Read.GetSymptoms());
 	}
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteResultToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteResultToFile.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class WriteResultToFile {
     public WriteResultToFile(Map<String, Integer> listSymp){
         try{
-        FileWriter writer = new FileWriter ("C:/Users/Thibault Carabasse/OneDrive - ITBRM Consulting/Bureau/OpenClassrooms/P4_Carabasse_Thibault/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/result.txt");
+        FileWriter writer = new FileWriter ("C:/Users/Thiba/Desktop/OpenClassrooms/P4/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/result.txt");
 		//For each line of symptom, write name of this symptom and his occurence in the new file
 		for(String symp : listSymp.keySet()){
 			writer.write(symp + " : " + listSymp.get(symp) + "\n");
